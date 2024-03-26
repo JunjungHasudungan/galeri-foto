25a80a51b619e02b03dae93af49e8624743958fc
junjunghasudungan <junjunghasudungan43@gmail.com>
2024-03-26 08:31:04 +0700

    handle store comment post

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -8,17 +8,18 @@ use App\Models\{Like, Post, Comment};
 class Dashboard extends Component
 {
     public  $is_create = false,
+            $isDisabled = false,
             $author,
             $post,
-            $isi_konten,
+            $konten,
+            $keterangan,
             $post_id,
             $created_at,
             $user_id;
-            
-    
+
+
     public $rules = [
         'konten' => 'required|min:2|max:50',
-        // 'gambar' => 'nullable|image|mimes:jpeg,png|max:2048', // Maksimum ukuran file gambar adalah 2MB
     ];
 
     public function render()
@@ -40,55 +41,54 @@ class Dashboard extends Component
         return $this->is_create = false;
     }
 
+    
+
     public function createCommentPost($id_post)
     {
         $this->openModalCreate();
 
+        $this->resetField();
+        
         $this->post = Post::with('user')->where('id', $id_post)->get();
 
         foreach($this->post as $post) {
+            $this->post = $post;
+            $this->post_id = $post->id;
             $this->author = $post->user;
             $this->created_at = $post->created_at;
-            $this->isi_konten = $post->keterangan;
+            $this->keterangan = $post->keterangan;
         }
-        
-        // dd($this->konten);
         $this->resetField();
     }
 
     public function resetField()
     {
         $this->user_id = '';
-        $this->isi_konten = '';
+        $this->konten = '';
         $this->post_id = '';
     }
 
 
-    public function storeCommentPost() 
+    public function storeCommentPost(Post $post)
     {
         // membuat validasi dari rules yang sudah dibuat
         $this->validate([
-            'konten' => 'required|min:2|max:50',
-            // 'keterangan' => 'required|min:10|max:225',
-            // 'gambar' => 'nullable|image|mimes:jpeg,png|max:2048', // Maksimum ukuran file gambar adalah 2MB
+            'konten'     => 'required|min:2|max:50',
         ],[
             'konten.required'    => 'judul wajib diisi..',
-            'judul.min'         => 'judul minimal 2 karakter',
-            'judul.max'         => 'judul minimal 2 karakter',
         ]);
-
-        // melakukan inisialisasi class objek
+        
         $comment = new Comment();
-
         // melakukan insert data dari pengiriman data properti yang ada
-        $comment = Comment::insert([
+        $comment = Comment::create([
             'konten'            => $this->konten,
             'user_id'           => auth()->user()->id,
-            'post_id'           => $this->post_id
+            'post_id'           =>  $post->id
         ]);
-
+        
         $comment->save();
 
+        
         $this->resetField();
 
         $this->closeModalCreate();
--- resources/views/livewire/pages/dashboard/_modalCreatePostComment.blade.php
+++ resources/views/livewire/pages/dashboard/_modalCreatePostComment.blade.php
@@ -10,9 +10,9 @@
 
           <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
             <form>
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <ol class="relative border-s border-gray-200 dark:border-gray-700">                  
-                        <li class="mb-10 ms-6">            
+                <div class="bg-white px-2 pt-5 pb-1 sm:p-6 sm:pb-1">
+                    <ol class="relative border-s border-gray-200 dark:border-gray-700">
+                        <li class="mb-3 ms-6">
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                                 <img class="rounded-full shadow-lg" src="{{ asset('assets/media/avatars/300-5.jpg') }}" alt="Thomas Lean image"/>
                             </span>
@@ -21,61 +21,59 @@
                                     {{$author->name }} . {{ \Carbon\Carbon::parse($created_at)->translatedFormat('j M') }}
                                 </p>
                             </div>
-                            <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
-                                @php
-                                    foreach($post as $item) {
-                                        $isi_kontent = $item->keterangan;
-                                    }
-                                @endphp
+                            <div class="items-center justify-between p-4 border border-gray-200 rounded-lg shadow-sm sm:flex bg-stone-400 dark:border-gray-600">
                                 <div class="text-sm font-normal text-gray-500 dark:text-gray-300">
-                                    {{ $isi_kontent }} 
+                                    {{ $post->keterangan }}
                                 </div>
                             </div>
                         </li>
-                        <li class="mb-10 ms-6">
+                        <li class="mb-3 ms-6">
                             <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                                 <img class="rounded-full shadow-lg" src="{{ asset('assets/media/avatars/300-4.jpg') }}" alt="Thomas Lean image"/>
                             </span>
-                            <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
+                            <div class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm  dark:border-gray-600">
                                 <div class="items-center justify-between mb-3 sm:flex">
-                                    <div class="text-sm font-normal text-gray-500 lex dark:text-gray-300">Membalas 
-                                        <span class="font-semibold text-gray-900 dark:text-white hover:underline">  {{ $author->name }} </span>
+                                    <div class="text-sm font-normal text-gray-500 lex dark:text-gray-400">Membalas
+                                        <span class="font-semibold text-gray-900  hover:underline">  {{ $author->name }} </span>
                                     </div>
                                 </div>
-                                <textarea   
-                                    id="message" 
-                                    rows="4" 
+                                <textarea
+                                    id="konten"
+                                    rows="4"
+                                    wire:model="konten"
                                     class="block p-2.5 w-full text-sm text-gray-700 rounded-lg border border-gray-600 focus:ring-blue-500 focus:border-blue-500 placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Postingn balasan anda...">
                                 </textarea>
+                                @error('konten') <span class="text-red-500">{{ $message }}</span>@enderror
                             </div>
                         </li>
                     </ol>
                 </div>
+                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                            <button
+                                    wire:click="storeCommentPost({{ $post->id }})"
+                                    type="button"
+                                    id="btnStoreCommentPost"
+                                    class="inline-flex bg-green-600 justify-center w-full rounded-md border border-transparent
+                                    px-4 py-2  text-base leading-6 font-medium text-white shadow-sm
+                                    focus:outline-none focus:border-green-700 focus:shadow-outline-green
+                                    transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Simpan
+                            </button>
+                        </span>
+                        <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                            <button wire:click="closeModalCreate()"
+                                    type="button"
+                                    class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2
+                                    bg-gray-600 text-base  text-white leading-6 font-medium text-whiteshadow-sm
+                                    hover:bg-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue
+                                    transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Cancel
+                            </button>
+                        </span>
+                    </div>
                 </div>
 
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-                        <button
-                                wire:click.prevent="storeCommentPost()"
-                                type="button"
-                                class="inline-flex justify-center w-full rounded-md border border-transparent
-                                px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm
-                                hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green
-                                transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                            Simpan
-                        </button>
-                    </span>
-                    <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-                        <button wire:click="closeModalCreate()"
-                                type="button"
-                                class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2
-                                bg-gray-600 text-base  text-white leading-6 font-medium text-whiteshadow-sm
-                                hover:bg-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue
-                                transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                            Cancel
-                        </button>
-                    </span>
-                </div>
             </form>
           </div>
         </div>
@@ -83,9 +81,27 @@
 </div>
 @push('scripts')
 <script>
+    // const inputKonten = document.getElementById('konten');
+    // const btnStoreComment = document.getElementById('btnStoreCommentPost');
+    // inputKonten.addEventListener("keyup", function(e){
+    //     btnStoreComment = .currentTarget.value;
+
+    //     if(value == "") {
+    //         btnStoreComment.disable = true;
+    //     }else {
+    //         btnStoreComment.disable = false;
+    //     }
+
+    // });
+
+    document.addEventListener('livewire:load', function () {
+        Livewire.on('toggleButton', (isDisabled) => {
+            window.livewire.myLivewireComponent.isDisabled = isDisabled;
+        });
+    });
     document.addEventListener('livewire:load', function () {
         const messageTextarea = document.querySelector('[x-ref="message"]');
-        @this.focusTextarea && messageTextarea.focus(); 
+        @this.focusTextarea && messageTextarea.focus();
     });
 </script>
 @endpush
